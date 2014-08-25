cdbd9484c0c9edc659e49a8cfc1f9d0e992718c0
Eduardo Pérez-Bermejo <ifedu@outlook.com>
2014-08-24 19:02:06 -0500

    0.1.5

--- tasks/js2cleanly.js
+++ tasks/js2cleanly.js
@@ -13,18 +13,20 @@ module.exports = function(grunt) {
 
     grunt.registerMultiTask('js2cleanly', 'js2cleanly for Grunt', function() {
         this.files.forEach(function (files) {
-			files = files.src.filter(function(filepath) {
-				return (!grunt.file.exists(filepath)) ? false : true;
-			});
+            var filesDest = files.dest;
 
-			for (var i = 0, length = files.length; i < length; i++) {
-				var file = files[i];
+            var filesSrc = files.src.filter(function(filepath) {
+                return (!grunt.file.exists(filepath)) ? false : true;
+            });
 
-				var dataFile = grunt.file.read(file);
-				dataFile = js2Cleanly(dataFile);
-				grunt.file.write(file + '-cleanly', dataFile);
-				grunt.log.writeln('File "' + file + '-cleanly' + '" created.');
-			}
-		});
+            for (var i = 0, length = filesSrc.length; i < length; i++) {
+                var fileSrc = filesSrc[i];
+
+                var dataFile = grunt.file.read(fileSrc);
+                dataFile = js2Cleanly(dataFile);
+                grunt.file.write(filesDest + fileSrc + '-cleanly', dataFile);
+                grunt.log.writeln('File "' + filesDest + fileSrc + '-cleanly' + '" created.');
+            }
+        });
     });
 };
